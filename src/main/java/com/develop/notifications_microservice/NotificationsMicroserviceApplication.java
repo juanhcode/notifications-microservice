27104f645ae6dea266e1c79bf600267bf11d5dda
Juan Hoyos <juanhoyos1347@gmail.com>
2025-04-06 02:40:34 -0500

    Feat: structure

---
+++ src/main/java/com/develop/notifications_microservice/NotificationsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.develop.notifications_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develop/notifications_microservice/NotificationsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.notifications_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
